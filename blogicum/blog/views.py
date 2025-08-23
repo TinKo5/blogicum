3537a9fdc67b254b6e0dde129b353f2aa887d1df
Martin K <nitrammok@gmail.com>
2025-08-23 16:37:12 +0300

    Created route and view function

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+
 # Create your views here.
+def index(request):
+    template = 'blog/index.html'
+    return render(request, template)
